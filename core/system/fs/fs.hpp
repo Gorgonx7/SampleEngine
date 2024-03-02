4f3d4cd626af0c1d93cd6cee505a23d472144edf
James Gordon <james.gordon1628@gmail.com>
2024-03-02 22:43:55 +0000

    convert to vsc

---
+++ core/system/fs/fs.hpp
@@ -0,0 +1,10 @@
+//
+//  fs.hpp
+//  SampleEngine
+//
+//  Created by James Gordon on 24/02/2024.
+//
+
+#pragma once
+
+
---
+++ shaders/Script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  Script.sh
+#  SampleEngine
+#
+#  Created by James Gordon on 23/02/2024.
+#
+glslc shader.vert -o vert.spv
+glslc shader.frag -o frag.spv
