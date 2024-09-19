a69881e7afb39a3f69425f1699e68686f6c31509
James Gordon <James.Gordon1628@gmail.com>
2024-09-19 20:08:17 +0100

    restructure includes

--- main.cpp
+++ main.cpp
@@ -12,9 +12,6 @@
 #include <cstdlib>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#define GLM_ENABLE_EXPERIMENTAL
 #include <array>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
