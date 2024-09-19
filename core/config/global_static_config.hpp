df6910489de19cf2ffc73767c702f1976915fddb
James Gordon <james.gordon1628@gmail.com>
2024-09-19 20:45:21 +0100

    Update cmake-multi-platform.yml (#2)
    * Update cmake-multi-platform.yml
    
    * Update cmake-multi-platform.yml
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update cmake-multi-platform.yml
    
    * Update cmake-multi-platform.yml
    
    * move glm to core subdir
    
    * restructure includes
    
    * update git module
    
    * fix glm
    
    * include limits
    
    * run on macos latest
    
    * fix macos
    
    * add gcc mac support

--- core/config/global_static_config.hpp
+++ core/config/global_static_config.hpp
@@ -1,5 +1,10 @@
 #pragma once
 #include <vector>
 #include <vulkan/vulkan.hpp>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+
 static const std::vector<const char *> device_extensions = {
     VK_KHR_SWAPCHAIN_EXTENSION_NAME};
