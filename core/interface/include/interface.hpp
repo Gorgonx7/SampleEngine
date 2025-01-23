7df0a26d93f1b80104269fe81d3b95cd625ce791
James Gordon <James.Gordon1628@gmail.com>
2025-01-23 18:56:32 +0000

    workshop

--- core/interface/include/interface.hpp
+++ core/interface/include/interface.hpp
@@ -1,12 +1,20 @@
 #pragma once
 #include <glfw/glfw3.h>
 #include "state.hpp"
+#include "renderpass.hpp"
+#include "command.hpp"
+#include "buffer.hpp"
 namespace interface
 {
     namespace config
     {
         void GenerateInstance(GLFWwindow *window, vk_state *state);
+        class Interface
+        {
+            /// @brief VkDescriptorPool, VkRenderPass, VkCommandBuffer, and VkFramebuffer
 
+            Interface();
+        };
     }
     namespace draw
     {
