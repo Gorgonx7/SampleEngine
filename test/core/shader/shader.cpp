269b916c371ac5286c08153e7b20f2953cbf1b9b
James Gordon <James.Gordon1628@gmail.com>
2025-08-17 16:54:27 +0100

    unit tests

--- test/core/shader/shader.cpp
+++ test/core/shader/shader.cpp
@@ -1,7 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
-#include "../../../core/shader/shader.hpp"
-
+#include <shader.hpp>
+#include <vk_fake.hpp>
 TEST_CASE("shader of 1 and higher are computed (pass)", "[shader-file]")
 {
+    FakeVulkan *vk = new FakeVulkan();
     // Shader shader = Shader("./test_files/shader.vert");
 }
--- test/helpers/vk_fake.hpp
+++ test/helpers/vk_fake.hpp
@@ -1,7 +1,18 @@
 #include <vulkan/vulkan.hpp>
+#include <instance.hpp>
+#include <surface.hpp>
+#include <device.hpp>
 
+struct FakeVulkan
+{
+    instance *i;
+    surface *s;
+    physical_device *p;
+};
 VkDevice get_fake_device()
 {
+    instance *i = new instance();
+    surface *s = new surface(i->get_instance(), nullptr);
 
     return nullptr;
 }
