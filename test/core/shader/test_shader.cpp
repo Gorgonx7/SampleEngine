f52155e42dc1c02aca6c869412d357ca5a77943a
James Gordon <james.gordon1628@gmail.com>
2025-08-24 19:57:07 +0100

    finish fake vulkan implementation

--- test/core/shader/test_shader.cpp
+++ test/core/shader/test_shader.cpp
@@ -5,10 +5,11 @@
 TEST_CASE("Shader abstraction can create a shader from shader.vert using vk_fake", "[vulkan][shader]")
 {
     // Setup fake Vulkan environment
-    FakeVulkan vk = make_fake_instance_device();
-    REQUIRE(vk.vk_logical_device != nullptr);
+    FakeVulkan *vk = make_fake_instance_device();
+    REQUIRE(vk->vk_logical_device != nullptr);
 
     // Create a Shader from shader.vert
-    Shader shader(vk.vk_logical_device->get_device(), VERTEX_SHADER, "shaders/shader.vert");
+    Shader shader(vk->vk_logical_device->get_device(), VERTEX_SHADER, "shaders/shader.vert");
     REQUIRE(shader.get_shader_module() != nullptr);
+    delete vk;
 }
--- test/helpers/vk_fake.hpp
+++ test/helpers/vk_fake.hpp
@@ -19,14 +19,18 @@
 
 struct FakeVulkan
 {
+    GLFWwindow * window = nullptr;
     instance *vk_instance = nullptr;
     physical_device *vk_physical_device = nullptr;
     logical_device *vk_logical_device = nullptr;
+    surface *vk_surface = nullptr;
     swapchain *vk_swapchain = nullptr;
     RenderPass *vk_renderpass = nullptr;
     GraphicsPipeline *vk_graphics_pipeline = nullptr;
     CommandPool *vk_command_pool = nullptr;
     Buffer *vk_buffer = nullptr;
+    VkQueue graphicsQueue = VK_NULL_HANDLE;
+    VkQueue presentQueue = VK_NULL_HANDLE;
 
     ~FakeVulkan()
     {
@@ -36,6 +40,8 @@ struct FakeVulkan
             delete vk_physical_device;
         if (vk_logical_device)
             delete vk_logical_device;
+        if (vk_surface)
+            delete vk_surface;
         if (vk_swapchain)
             delete vk_swapchain;
         if (vk_renderpass)
@@ -46,31 +52,55 @@ struct FakeVulkan
             delete vk_command_pool;
         if (vk_buffer)
             delete vk_buffer;
+        if (window){
+            glfwDestroyWindow(window);
+            glfwTerminate();
+        }
     }
 };
-
-inline FakeVulkan make_fake_instance_device()
+inline GLFWwindow* make_GLFW(){
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+    return glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+}
+inline FakeVulkan * make_fake_instance_device()
 {
-    FakeVulkan vk;
-    vk.vk_instance = new instance();
-    vk.vk_physical_device = new physical_device(vk.vk_instance->get_instance(), VK_NULL_HANDLE);
-    vk.vk_logical_device = new logical_device(vk.vk_instance, vk.vk_physical_device->get_device(), VK_NULL_HANDLE, false, nullptr, nullptr);
+    FakeVulkan * vk = new FakeVulkan();
+    vk->window = make_GLFW();
+    vk->vk_instance = new instance();
+    vk->vk_surface = new surface(vk->vk_instance->get_instance(), vk->window);
+    vk->vk_physical_device = new physical_device(vk->vk_instance->get_instance(), vk->vk_surface->get_surface());
+    vk->vk_logical_device = new logical_device(
+        vk->vk_instance,
+        vk->vk_physical_device->get_device(),
+        vk->vk_surface->get_surface(),
+        false,
+        &vk->graphicsQueue,
+        &vk->presentQueue
+    );
     return vk;
 }
 
-inline FakeVulkan make_fake_swapchain_renderpass(GLFWwindow *window)
+inline FakeVulkan *make_fake_swapchain_renderpass(GLFWwindow *window)
 {
-    FakeVulkan vk = make_fake_instance_device();
-    vk.vk_swapchain = new swapchain(window, vk.vk_physical_device->get_device(), vk.vk_logical_device->get_device(), VK_NULL_HANDLE, VK_SAMPLE_COUNT_1_BIT);
-    vk.vk_renderpass = new RenderPass(vk.vk_physical_device->get_device(), VK_SAMPLE_COUNT_1_BIT, vk.vk_logical_device->get_device(), VK_FORMAT_B8G8R8A8_UNORM, VK_FORMAT_D32_SFLOAT);
+    FakeVulkan * vk = make_fake_instance_device();
+    vk->vk_swapchain = new swapchain(window, vk->vk_physical_device->get_device(), vk->vk_logical_device->get_device(), vk->vk_surface->get_surface(), VK_SAMPLE_COUNT_1_BIT);
+    vk->vk_renderpass = new RenderPass(vk->vk_physical_device->get_device(), VK_SAMPLE_COUNT_1_BIT, vk->vk_logical_device->get_device(), VK_FORMAT_B8G8R8A8_UNORM, VK_FORMAT_D32_SFLOAT);
     return vk;
 }
 
-inline FakeVulkan make_fake_full(GLFWwindow *window)
+inline FakeVulkan * make_fake_full(GLFWwindow *window)
 {
-    FakeVulkan vk = make_fake_swapchain_renderpass(window);
-    vk.vk_graphics_pipeline = new GraphicsPipeline(vk.vk_logical_device->get_device(), VK_SAMPLE_COUNT_1_BIT, vk.vk_renderpass->get_render_pass(), VK_NULL_HANDLE);
-    vk.vk_command_pool = new CommandPool(vk.vk_physical_device->get_device(), VK_NULL_HANDLE, vk.vk_logical_device->get_device());
-    vk.vk_buffer = new Buffer(vk.vk_logical_device->get_device(), vk.vk_physical_device->get_device(), 1024, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+    FakeVulkan * vk = make_fake_swapchain_renderpass(window);
+    vk->vk_graphics_pipeline = new GraphicsPipeline(
+        vk->vk_logical_device->get_device(),
+
+        VK_SAMPLE_COUNT_1_BIT,
+        vk->vk_renderpass->get_render_pass(),
+    VK_NULL_HANDLE);
+    vk->vk_command_pool = new CommandPool(vk->vk_physical_device->get_device(), vk->vk_surface->get_surface(), vk->vk_logical_device->get_device());
+    vk->vk_buffer = new Buffer(vk->vk_logical_device->get_device(), vk->vk_physical_device->get_device(), 1024, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
     return vk;
 }
